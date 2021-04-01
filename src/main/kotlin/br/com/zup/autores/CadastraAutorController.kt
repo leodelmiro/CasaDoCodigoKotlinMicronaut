ad16c3659593dcb6aed8e0bfdecb93b4ee7dcdaa
Leonardo Delmiro <leodelmiroms@gmail.com>
2021-04-01 15:49:10 -0300

    Created + UriBuild para cadastro de autor

--- src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -1,24 +1,30 @@
 package br.com.zup.autores
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.transaction.Transactional
 import javax.validation.Valid
 
 @Validated
 @Controller("/autores")
-class CadastraAutorController(val autorRepository: AutorRepository) {
+class CadastraAutorController(val autorRepository: AutorRepository){
 
     @Post
     @Transactional
-    fun cadastra(@Body @Valid request: NovoAutorRequest) {
+    fun cadastra(@Body @Valid request: NovoAutorRequest): HttpResponse<Any> {
         println("Requisição => $request")
 
         val autor = request.paraAutor()
-        autorRepository.save(autor)
 
         println("Autor => ${autor.nome}")
+        autorRepository.save(autor)
+
+        val uri = UriBuilder.of("/autores/{id}").expand(mutableMapOf(Pair("id", autor.id)))
+
+        return HttpResponse.created(uri)
     }
 }
